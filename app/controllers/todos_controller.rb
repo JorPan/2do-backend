409191f3c2334cbe96eb509279183b0271652137
JorPan <jbpanasewicz@gmail.com>
2021-02-26 16:11:23 -0700

    full auth locked down

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -41,7 +41,7 @@ class TodosController < ApplicationController
     end
 
     def todo_params
-        params.require(:todo).permit(:title, :content, :urgent, :done)
+        params.require(:todo).permit(:title, :content, :urgent, :done, :user_id)
     end
 
 end
