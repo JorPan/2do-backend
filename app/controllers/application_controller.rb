40b06c92a242b0b4d5daa53126ad0151edf2ffb9
JorPan <jbpanasewicz@gmail.com>
2021-02-26 12:18:17 -0700

    login and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+    
+    def login
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            @token = JWT.encode({user_id: @user.id}, "Secret_goes_here")
+            render json: {user: @user, token: @token }, status: :accepted
+        else
+            render json: {errors: ["Invalid username or password"]}, status: :unauthorized
+        end
+    end
+
+
+
+
 end
